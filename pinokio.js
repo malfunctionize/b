5625414bc661daaaa944310f1e5c254a3afd30a2
malfunctionize <malfunctionize@protonmail.com>
2023-06-11 17:22:26 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -1,5 +1,9 @@
-{
-  "install": "tumblr.json",
+module.exports = {
+  menu: [{
+    text: "<i class='fa-brands fa-tumblrx'></i> Configure Tumblr",
+    type: "link",
+    href: "tumblr.json"
+  }],
   "dependencies": [
     "https://github.com/malfunctionize/llama.git",
     "https://github.com/malfunctionize/automatic1111.git",
